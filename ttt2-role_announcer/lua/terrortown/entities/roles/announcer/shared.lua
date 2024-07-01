e3fe562a2e749f2f9c18a965f96f2ace1985ec97
milkwxter <ethan.r.bates2003@gmail.com>
2024-06-30 21:57:36 -0500

    Finished role

--- ttt2-role_announcer/lua/terrortown/entities/roles/announcer/shared.lua
+++ ttt2-role_announcer/lua/terrortown/entities/roles/announcer/shared.lua
@@ -5,7 +5,7 @@ if SERVER then
 end
 
 function ROLE:PreInitialize()
-	self.color = Color(7, 167, 174, 255)
+	self.color = Color(75, 104, 169, 255)
 
 	self.abbr = "announcer"
 	self.score.killsMultiplier = 8
@@ -49,7 +49,15 @@ if SERVER then
 	end
 
 	-- When round begins reset cooldowns to prevent funky business
-	hook.Add("TTT2OrderedEquipment", "AnnouncerSomeonePurchased", function()
-		print(equipmentName)
+	hook.Add("TTT2OrderedEquipment", "AnnouncerSomeonePurchased", function(ply, equipmentName, isItem, credits, ignoreCost)
+		-- find the announcer
+		for k, v in pairs(roles.GetTeamMembers(TEAM_INNOCENT)) do
+			-- make sure he is alive
+			if v:GetRoleString() == "announcer" and v:Alive() then
+				-- print message to everyones screen
+				EPOP:AddMessage(nil, {text = "Someone has purchased an item: " .. equipmentName, color = ANNOUNCER.color}, "This message was broadcasted by your Announcer", 5, true)
+				return
+			end
+		end
 	end)
 end
